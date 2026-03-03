f6d41c21f2025ab4077b04ccd550192919083071
Fortune505 <forturn397@gmail.com>
2026-03-03 12:53:40 +0800

    GUI versi 1

--- GUI/Form1.Designer.cs
+++ GUI/Form1.Designer.cs
@@ -28,12 +28,88 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            textBoxSquare = new TextBox();
+            label1 = new Label();
+            label2 = new Label();
+            textBoxCircle = new TextBox();
+            buttonCompare = new Button();
+            labelResult = new Label();
+            SuspendLayout();
+            // 
+            // textBoxSquare
+            // 
+            textBoxSquare.Location = new Point(12, 37);
+            textBoxSquare.Name = "textBoxSquare";
+            textBoxSquare.Size = new Size(183, 27);
+            textBoxSquare.TabIndex = 0;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(12, 14);
+            label1.Name = "label1";
+            label1.Size = new Size(183, 20);
+            label1.TabIndex = 1;
+            label1.Text = "Ведите сторону квадрата";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(12, 77);
+            label2.Name = "label2";
+            label2.Size = new Size(150, 20);
+            label2.TabIndex = 2;
+            label2.Text = "Ведите радиус круга";
+            // 
+            // textBoxCircle
+            // 
+            textBoxCircle.Location = new Point(12, 100);
+            textBoxCircle.Name = "textBoxCircle";
+            textBoxCircle.Size = new Size(183, 27);
+            textBoxCircle.TabIndex = 3;
+            // 
+            // buttonCompare
+            // 
+            buttonCompare.Location = new Point(12, 146);
+            buttonCompare.Name = "buttonCompare";
+            buttonCompare.Size = new Size(183, 59);
+            buttonCompare.TabIndex = 4;
+            buttonCompare.Text = "Нажми на кнопку получишь результат";
+            buttonCompare.UseVisualStyleBackColor = true;
+            // 
+            // labelResult
+            // 
+            labelResult.AutoSize = true;
+            labelResult.Location = new Point(12, 217);
+            labelResult.Name = "labelResult";
+            labelResult.Size = new Size(78, 20);
+            labelResult.TabIndex = 5;
+            labelResult.Text = "Результат:";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(293, 319);
+            Controls.Add(labelResult);
+            Controls.Add(buttonCompare);
+            Controls.Add(textBoxCircle);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(textBoxSquare);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private TextBox textBoxSquare;
+        private Label label1;
+        private Label label2;
+        private TextBox textBoxCircle;
+        private Button buttonCompare;
+        private Label labelResult;
     }
 }
---
+++ GUI/Logic.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GUI
+{
+    public class Logic
+    {
+        public static double GetSKv(double a) => a * a;
+
+        public static double GetSKr(double r)
+        {
+            const double pi = 3.14;
+            return r * r * pi;
+        }
+    }
+}
